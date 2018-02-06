a7352039f8f1e161a3500b59d24ee2d2296ce593
alkoral <35643632+alkoral@users.noreply.github.com>
2018-02-06 14:56:04 +0300

    Add files via upload
    Домашнее задание к лекции 1.3 «Строки, массивы и объекты» (исправленное)

--- lesson_1-3.php
+++ lesson_1-3.php
@@ -1,3 +1,52 @@
+<?php
+
+// Задание 1: Составить массив, ключами в котором будут названия континентов,
+// значениями — массивы из латинских названий зверей.
+// Решение: Создаем двумерный массив
+
+$continents=[
+    "Africa"=> array("Giraffa camelopardalis", "Loxodonta africana", "Equus zebra zebra", "Panthera leo", "Pan troglodytes troglodytes", "Graphiurus crassicaudatus", "Gazella gazella", "Hippopotamus amphibius", "Bufotes viridis", "Pleurodeles waltl", "Dendroaspis polylepis", "Testudo kleinmanni", "Varanus", "Phoenicopterus roseus", "Crocodylus niloticus"),
+    "Asia" => array("Dryomys nitedula", "Tamias sibiricus", "Lagurus lagurus", "Talpa altaica"),
+    "Australia" => array("Macropus", "Arctocephalus pusillus", "Rattus lutreolus", "Chelodina longicollis", "Canis lupus dingo"),
+    "Europe" => array("Meles meles", "Spermophilus pygmaeus", "Ursus arctos", "Pipistrellus pipistrellus"),
+    "North America" => array("Odocoileus", "Bison bison bison", "Ursus arctos horribilis", "Conepatus leuconotus"),
+    "South America" => array("Lama glama", "Eunectes murinus", "Serrasalmus rhombeus")
+];
+
+// Задание 2: Найдите всех зверей, название которых состоит из двух слов.
+// Составьте из них новый массив.
+// Решение: формируем массивы из зверей с двуми словами в названии
+
+$double_animals = [];
+
+foreach ($continents as $region=>$animals) {
+//  echo "<br><h2>".$region."</h2><br>";
+        foreach ($animals as $animal) {
+            if (str_word_count($animal)==2) {
+//          echo $animal.", ";
+            $double_animals[]=$animal;
+            $creatures = explode(" ", $animal);
+            $animals_1[]=$creatures[0];
+            $animals_2[]=$creatures[1];
+            }
+        }
+}
+
+//  Задание 3: Случайно перемешайте между собой первые и вторые слова названий
+//  животных так, чтобы на выходе мы получили выдуманных, фантазийных животных.
+//  Решение: а) перемешиваем массив с названиями зверей...
+
+shuffle($animals_2);
+
+// б) создаем массив с фантастическими животными
+
+$fantastic = [];
+    for ($i=0; $i<=count($animals_1)-1; $i++) {
+        $fantastic[] = $animals_1[$i]." ".$animals_2[$i];
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -7,7 +56,7 @@
 <style>
     html, body {
         font-size: 15px;
-        font-family: Arial, Helvetica, sans-serif;
+        font-family: "Trebuchet MS", Helvetica, sans-serif;
     }
     h2 {
         font-size: 20px;
@@ -19,7 +68,7 @@
         position: relative;
         margin: 0 auto;
         max-width: 900px;
-        padding-bottom: 30px;
+        padding: 30px auto 20px auto;
         border-bottom: 1px solid red;
     }
 
@@ -28,7 +77,8 @@
         position: relative;
         margin: 0 auto;
         text-align: center;
-        font-family: Arial, Helvetica, sans-serif;
+        padding-top: 30px;
+        font-family: "Trebuchet MS", Helvetica, sans-serif;
         font-size: 18px;
         font-weight: 600;
     }
@@ -36,12 +86,9 @@
     .content {
         display: block;
         position: relative;
-        font-family: "Trebuchet MS", Helvetica, sans-serif;
-        font-size: 15px;
-        font-weight: 400;
-        padding: 30px 120px;
+        padding: 30px 80px;
         column-count: 2;
-        column-width: 300px;
+        column-width: 340px;
     }
 
     .animals {
@@ -49,14 +96,14 @@
         display: block;
         margin: 0 auto;
         width: 900px;
-        padding-top: 60px;
+        padding-top: 20px;
         border-bottom: 1px solid red;
     }
 
     .animals::after {
-      content: "";
-      display: block;
-      clear: both;
+        content: "";
+        display: block;
+        clear: both;
     }
 
     .animals div {
@@ -64,94 +111,58 @@
         float: left;
         width: 50%;
         vertical-align: top;
+        padding-top: 10px;
         padding-left: 60px;
-        padding-bottom: 40px;
+        padding-bottom: 30px;
         box-sizing: border-box;
     }
-
+    pre {
+        margin: 0;
+        font-family: "Trebuchet MS", Helvetica, sans-serif;
+        font-size: 15px;
+        font-weight: 400;
+        letter-spacing: 1.3px;
+    }
 </style>
-
-<?php
-
-// Выводим континенты и зверей
-
-$continents=[
-    "Africa"=> array("Giraffa camelopardalis", "Loxodonta africana", "Equus zebra zebra", "Panthera leo", "Pan troglodytes troglodytes", "Graphiurus crassicaudatus", "Gazella gazella", "Hippopotamus amphibius", "Bufotes viridis", "Pleurodeles waltl", "Dendroaspis polylepis", "Testudo kleinmanni", "Varanus", "Phoenicopterus roseus", "Crocodylus niloticus"),
-    "Asia" => array("Dryomys nitedula", "Tamias sibiricus", "Lagurus lagurus", "Talpa altaica"),
-    "Australia" => array("Macropus", "Arctocephalus pusillus", "Rattus lutreolus", "Chelodina longicollis", "Canis lupus dingo"),
-    "Europe" => array("Meles meles", "Spermophilus pygmaeus", "Ursus arctos", "Pipistrellus pipistrellus"),
-    "North America" => array("Odocoileus", "Bison bison bison", "Ursus arctos horribilis", "Conepatus leuconotus"),
-    "South America" => array("Lama glama", "Eunectes murinus", "Serrasalmus rhombeus")
-];
-
-// Формируем массив из зверей с двуми словами в названии
-
-$double_animals = [];
-
-foreach ($continents as $region=>$animals) {
-//  echo "<br><h2>".$region."</h2><br>";
-        foreach ($animals as $animal) {
-        if (str_word_count($animal)==2) {
-//          echo $animal.", ";
-        $double_animals[]=$animal;
-        }
-    }
-}
-
-// Выделяем в настоящих названиях "имена" и "фамилии" зверей
-
-$animals_1 = [];
-$animals_2 = [];
-
-foreach ($double_animals as $ani) {
-    list ($ani_1, $ani_2) = explode(" ", $ani);
-    $animals_1[]=$ani_1;
-    $animals_2[]=$ani_2;
-}
-
-// Перемешиваем массив с "фамилиями" зверей
-
-shuffle($animals_2);
-
-// Создаем массив с фантастическими животными
-
-$fantastic = [];
-    for ($i=0; $i<=count($animals_1)-1; $i++) {
-        $fantastic[] = $animals_1[$i]." ".$animals_2[$i];
-    }
-
-?>
 
 </head>
 <body>
-<?php
-echo "<pre>";
+    <section class='continents'>
+        <div class='title'>
+            1. All Continents And Animals:
+        </div>
+        <div class='content'>
+            <?php
+                echo "<pre>";
+                print_r($continents);
+                echo "</pre>";
+            ?>
+        </div>
 
-    echo "<section class='continents'>";
-    echo "<div class='title'>1. All Continents & Animals:</div>";
-    echo "<div class='content'>";
-    print_r($continents);
-    echo "</div>";
-    echo "</section>";
-    echo "<section class='animals'>";
-    echo "<div class='title'>";
-    echo "2. Real Two-Word Animals";
-    echo "</div>";
-    echo "<div class='title'>";
-    echo "3. Fantastic Animals";
-    echo "</div>";
-    echo "<div class='real'>";
-    print_r($double_animals);
-    echo "</div>";
-    echo "<div class='fanta'>";
-    print_r($fantastic);
-    echo "</div>";
-    echo "</section>";
+    </section>
 
-/*  Дополнительные задания постараюсь сделать после
-    более глубокого изучения темы замен в массивах, ок?
-*/  
-?>
+    <section class='animals'>
+        <div class='title'>
+            2. Real Two-Word Animals
+        </div>
+        <div class='title'>
+            3. Fantastic Animals
+        </div>
+        <div class='real'>        
+            <?php
+                echo "<pre>";
+                print_r($double_animals);
+                echo "</pre>";
+            ?>
+        </div>
+        <div class='fanta'>
+            <?php
+                echo "<pre>";
+                print_r($fantastic);
+                echo "</pre>";
+            ?>
+        </div>
+    </section>
 
 </body>
 </html>
